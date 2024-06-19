519510f3bfd26341c11ff57c824576ea029f4b9f
Alessio <alepoggejakad@gmail.com>
2024-06-19 13:38:24 +0200

    Collect static files

--- projectBE/settings.py
+++ projectBE/settings.py
@@ -96,9 +96,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Questa configurazione aggiuntiva è per includere file statici presenti in altre directory (se necessario)
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
